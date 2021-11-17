eed5decc5eb36bacdb3af4586580cc68fb4beef9
Suhail0310000 <s341868@oslomet.no>
2021-11-17 15:16:50 +0100

    geodata form done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,99 @@
 import './App.css';
-// import { useState } from 'react'
+import { useState } from 'react'
 
 function App() {
+  const [fornavn, setFornavn] = useState('')
+  const [etternavn, setEtternavn] = useState('')
+  const [email, setEmail] = useState('')
+  const [passord, setPassord] = useState('')
+  const [nyPassord, setnyPassord] = useState('')
+  const [checkbox, setCheckbox] = useState(false)
+  const [formError, setFormError] = useState(null)
+  // const [formError, setFormError] = useState(null)
+  const resetForm = () => {
+    setFornavn('')
+    setEtternavn('')
+    setEmail('')
+    setPassord('')
+    setnyPassord('')
+    setCheckbox(false)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormError(null)
+    const entry = {
+      fornavn: fornavn,
+      etternavn: etternavn,
+      email: email,
+      passord: passord,
+      nyPassord: nyPassord
+    }
+
+    //Inputvalidering
+    // if(fornavn.length < 1){
+    //   console.log('Skriv inn fornavn for å registrere deg')
+    //   setFormError('ERROR! Skriv inn fornavn for å registrere deg')
+    //   return
+    // }
+    // if(etternavn.length < 1){
+    //   console.log('Skriv inn etternavn for å registrere deg')
+    //   setFormError('ERROR! Skriv inn etternavn for å registrere deg')
+    //   return
+    // }
+    // if(email.length < 1){
+    //   console.log('Skriv inn email for å registrere deg')
+    //   setFormError('ERROR! Skriv inn email for å registrere deg')
+    //   return
+    // }
+    if(passord.length < 1){
+      console.log('Skriv inn passord for å registrere deg')
+      setFormError('ERROR! Skriv inn passord for å registrere deg')
+      return
+    }
+    if(nyPassord.length < 1){
+      console.log('gjenta passordet ditt, dersom du ønsker å  registrere deg')
+      setFormError('ERROR! gjenta passordet ditt, dersom du ønsker å  registrere deg')
+      return
+    }
+    if(passord.trim() !== nyPassord){
+      console.log('passord må være like')
+      setFormError('ERROR! gjenta passordet ditt, dersom du ønsker å  registrere deg')
+      return
+    }
+    if(checkbox === false){
+      console.log('Check av selectbox for å registrere deg!')
+      setFormError('ERROR! Check av selectbox for å registrere deg!')
+      return
+    }
+    fetch(`${window.origin}/register`, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify(entry),
+      cache: "no-cache",
+      headers: new Headers({
+          "content-type": "application/json"
+      })
+    })
+    .then(function (response){
+        if(response.status !== 200){
+            console.log(`Response status was not 200: ${response.status}`);
+            return;
+        }
+        response.json().then(function(data){
+            console.log(data)
+        })
+    })
+    console.log(entry)
+    resetForm()
+  }
+  // const [fornavn, setFornavn] = useState('')
+  // const [fornavn, setFornavn] = useState('')
+  // const [fornavn, setFornavn] = useState('')
+
+  // const handleChange = (e) => {
+  //   console.log(e.target.value)
+  // }
   return (
     <div className="flex-container">
       <div className="flex-item-left">
@@ -14,34 +106,41 @@ function App() {
       </div>
       <div className="flex-item-right">
         <h3>Opprett bruker</h3>
-        <form className="register-form">
+        <form className="register-form" onSubmit={handleSubmit}>
           <div className="div1">
             <label>
               <p>Fornavn</p>
-              <input type="text"/>
+              <input required type="text" label="password" onChange={(e) => setFornavn(e.target.value)} value={fornavn}/>
             </label>
             <label style={{marginLeft:"2%"}}>
               <p>Etternavn</p>
-              <input type="text"/>
+              <input required type="text" onChange={(e) => setEtternavn(e.target.value)} value={etternavn}/>
             </label>
           </div>  
           <label>
               <p>E-postadresse</p>
-              <input type="email"/>
+              <input required type="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
           </label>
           <div>
             <label>
               <p style={{margin:"0px"}}>Passord</p>
-              <input type="text"/>
+              <input type="text" data-testid="pass1" label="textbox" onChange={(e) => setPassord(e.target.value)} value={passord}/>
             </label>
             <label style={{marginLeft:"2%"}}>
               <p style={{margin:"0px"}}>Gjenta passord</p>
-              <input type="text"/>
+              <input type="password" data-testid="pass2" onChange={(e) => setnyPassord(e.target.value)} value={nyPassord}/>
             </label>
           </div> 
-          <input type="checkbox"></input>
-          <label> Jeg godtar databehandlingsavtalen</label>
-          <button>Registrer</button>
+          <input type="checkbox" onChange={(e) => setCheckbox(e.target.value)} value={checkbox}></input>
+          <label> Jeg godtar <a href="">databehandlingsavtalen</a></label>
+          <button data-testid="button">Registrer</button>
+          {formError && <p className="error" data-testid="errormsg">{formError}</p>}
+          {/* <p>Fornavn {fornavn}</p>
+          <p>Etternavn {etternavn}</p>
+          <p>Email {email}</p>
+          <p>Passord {passord}</p>
+          <p>Gjenta passord {nyPassord}</p> */}
+          {/* <p onClick={resetForm}>Resetform</p> */}
         </form>
       </div>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("passord er like", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.change(screen.getByTestId("pass1"), {
+    target: { value: "123" },
+  });
+  fireEvent.change(screen.getByTestId("pass2"), {
+    target: { value: "123" },
+  });
+  fireEvent.click(screen.getByTestId("button"));
+  expect(screen.getByTestId("errormsg")).toHaveTextContent(
+    //Grunnet inputvalidering, så forventer vi dette
+    "ERROR! Check av selectbox for å registrere deg!"
+  );
 });
+
+
+test("passordene er ikke like", () => {
+  render(<App />);
+  fireEvent.change(screen.getByTestId("pass1"), {
+    target: { value: "123" },
+  });
+  fireEvent.change(screen.getByTestId("pass2"), {
+    target: { value: "12345" },
+  });
+  fireEvent.click(screen.getByTestId("button"));
+  expect(screen.getByTestId("errormsg")).toHaveTextContent(
+    "ERROR! gjenta passordet ditt, dersom du ønsker å registrere deg"
+  );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
